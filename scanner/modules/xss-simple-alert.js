8717d41653a4a85bfa1949390496173c103e0a01
Tomás Pinho <me@tomaspinho.com>
2014-12-03 10:39:21 +0000

    Seems to be fixed for vulnweb

--- scanner/modules/xss-simple-alert.js
+++ scanner/modules/xss-simple-alert.js
@@ -24,6 +24,7 @@ module.exports = {
 			// Get all forms in page via CSS selector powered by Cheerio
 			$ = cheerio.load(body);
 			var forms = $("form");
+
 			// Iterate through all forms and test them individually
 			forms.each(function (index, form) {
 				
@@ -31,44 +32,40 @@ module.exports = {
 				var method = $(this).attr("method") || "GET";
 				var inputs = $(this).children(":input");
 				var actionUrlObject = Url.resolve(urlObject.format(), action);
-				
 				// Iterate through all inputs and test them individually
 				var inputsArray = inputs.toArray();
-				for(var testingInput in inputsArray){
-					var javaScript = "<script type=“text/javascript”>alert('xss test');</script>";
+				inputsArray.forEach(function(index, testingInput){
+					var testingInputName = $(inputsArray[testingInput]).attr("name");
+
+					var javaScript = "<script type='text/javascript'>alert('xss test');</script>";
 					var form = {};
 					for(var input in inputsArray){
-						var inject = $(inputsArray[input]).attr("name") == $(inputsArray[testingInput]).attr("name"); 
+						var inject = $(inputsArray[input]).attr("name") == testingInputName; 
 						form[$(inputsArray[input]).attr("name")] = (inject ? javaScript : ($(inputsArray[input]).val() == "" || $(inputsArray[input]).val() == undefined ? "default" : $(inputsArray[input]).val()));
 					}
 					if(method == "GET"){
-						console.log("Sending GET");
-						console.log(form);
 						var actionWithQuery = actionUrlObject + "?" + require('querystring').stringify(form);
-						console.log(actionWithQuery);
 						request({url: actionWithQuery}, function(err,httpResponse,body){
-							console.log(JSON.stringify(httpResponse));
-							console.log(body);
 							if(body.toLowerCase().indexOf("<script type='text/javascript'>alert('xss test');</script>".toLowerCase()) > -1){
-								if(err){
-									console.log("XSS Simple Alert Module: Input with name " + $(inputsArray[testingInput]).attr("name") + " seems to be vulnerable to SQL Injection");
+								if(!err){
+									console.log("XSS Simple Alert Module: Input with name " + testingInputName + " seems to be vulnerable to XSS Injection");
 								} else {
-									console.log("XSS Simple Alert Module: Testing input with name " + $(inputsArray[testingInput]).attr("name") + " caused error " + err + " on the server");
+									console.log("XSS Simple Alert Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
 								}
 							}
 						});
 					} else {
 						request.post({url: actionUrlObject, form: form}, function(err,httpResponse,body){
 							if(body.toLowerCase().indexOf("<script type='text/javascript'>alert('xss test');</script>".toLowerCase()) > -1){
-								if(err){
-									console.log("XSS Simple Alert Module: Input with name " + $(inputsArray[testingInput]).attr("name") + " seems to be vulnerable to SQL Injection");
+								if(!err){
+									console.log("XSS Simple Alert Module: Input with name " + testingInputName + " seems to be vulnerable to XSS Injection");
 								} else {
-									console.log("XSS Simple Alert Module: Testing input with name " + $(inputsArray[testingInput]).attr("name") + " caused error " + err + " on the server");
+									console.log("XSS Simple Alert Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
 								}
 							}
 						});
 					}
-				}
+				});
 			});
 		}
 
