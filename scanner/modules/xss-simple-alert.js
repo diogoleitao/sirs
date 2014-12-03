daf932e74ab529a8327fe85fb1e24afb602bf174
Diogo Leitão <diogo.n.leitao@gmail.com>
2014-12-03 01:01:32 +0000

    changed 'vulnerableforms' loop to a better one, safer

--- scanner/modules/xss-simple-alert.js
+++ scanner/modules/xss-simple-alert.js
@@ -10,7 +10,7 @@ var request = require('sync-request');
 var cheerio = require("cheerio");
 var utilities = require("../utilities/");
 module.exports = {
-	run: function(url){
+	run: function (url) {
 		var injectableForms = [];
 
 		// Parsing the url and the queryString arguments
@@ -25,7 +25,7 @@ module.exports = {
 			var forms = $("form");
 
 			// Iterate through all forms and test them individually
-			forms.each(function(index, form){
+			forms.each(function (index, form) {
 				
 				var action = $(this).attr("action");
 				var method = $(this).attr("method");
@@ -34,24 +34,26 @@ module.exports = {
 				
 				// Iterate through all inputs and test them individually
 				var inputsArray = inputs.toArray();
-				for(var testingInput in inputsArray){
+				for (var testingInput in inputsArray) {
 					//console.log(inputsArray[testingInput]);
 					// Construct Request Body (form-data)
 					var formBody = "";
 					var javaScript = "<script type=“text/javascript”>alert('xss test');</script>";
-					for(var input in inputsArray){
-						var prependAmpersand = (formBody.length != 0);
-						var inject = $(inputsArray[input]).attr("name") == $(inputsArray[testingInput]).attr("name"); 
+					for (var input in inputsArray) {
+						var prependAmpersand = (formBody.length !== 0);
+						var inject = $(inputsArray[input]).attr("name") === $(inputsArray[testingInput]).attr("name"); 
 						formBody += (prependAmpersand ? "&" : "");
-						formBody += $(inputsArray[input]).attr("name") + "=" + (inject ? javaScript : ($(inputsArray[input]).val() == "" || $(inputsArray[input]).val() == undefined ? "default" : $(inputsArray[input]).val()));
+						formBody += $(inputsArray[input]).attr("name") + "=" + (inject ? javaScript : ($(inputsArray[input]).val() === "" || $(inputsArray[input]).val() === undefined ? "default" : $(inputsArray[input]).val()));
 					}
-					var res_form = request(method, urlObject.format(), { headers: {
-						"Content-Type": "application/x-www-form-urlencoded"
-					}, 
-					body: formBody});
-					if(res_form.getBody().toString().toLowerCase().indexOf("<script type=“text/javascript”>alert('xss test');</script>".toLowerCase()) > -1){
+					var res_form = request(method, urlObject.format(), {
+						headers: {
+							"Content-Type": "application/x-www-form-urlencoded"
+						}, 
+						body: formBody
+					});
+					if (res_form.getBody().toString().toLowerCase().indexOf("<script type=“text/javascript”>alert('xss test');</script>".toLowerCase()) > -1) {
 						// XSS injection found
-						if(injectableForms[actionUrlObject] == undefined){
+						if (injectableForms[actionUrlObject] === undefined) {
 							injectableForms[actionUrlObject] = [];
 						}
 						injectableForms[actionUrlObject].push($(inputsArray[testingInput]).attr("name"));
@@ -62,21 +64,20 @@ module.exports = {
 			//console.log(injectableForms);
 			// Produce result string
 			var resultString = "XSS Simple Alert Module: ";
-			if(injectableForms.length == 0){
+			if (injectableForms.length === 0) {
 				resultString += "no vulnerable forms found on page.";
 			} else {
 				resultString += "\n";
 			}
 
-			for(var form in injectableForms){
+			injectableForms.forEach(function (index, form) {
 				resultString += "\tIn form which action is " + form + "\n";
 				resultString += "\t\t Variables " + utilities.arrayToCommaSeparatedString(injectableForms[form]) + " seem to be vulnerable.";
-			}
+			});
 
 			return resultString;
 		} else {
-			return "XSS Simple Alert module: there was an error loading the page."
+			return "XSS Simple Alert module: there was an error loading the page.";
 		}
-			
 	}
 }
