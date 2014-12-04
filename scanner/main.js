57def7b789f3d9b413d8795987a261f104c9b7f8
theburdencarrier <theburdencarrier@TheBurdriersMBP.home>
2014-12-04 23:58:45 +0000

    Adding literature to scanner

--- scanner/main.js
+++ scanner/main.js
@@ -26,7 +26,10 @@ rl.on('close', function(){
 /* Setting up available scan modules */
 var scanModules = require('./modules/');
 console.log("Available scan modules: " + utilities.objectToCommaSeparatedString(scanModules));
-
+console.log("If vulnerabilities are found, we recommend the reading of these great articles by OWASP:");
+console.log("SQL Injection Prevention: https://www.owasp.org/index.php/SQL_Injection_Prevention_Cheat_Sheet");
+console.log("XSS Prevention: https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet");
+console.log("SSI Prevention: https://www.owasp.org/index.php/Server-Side_Includes_%28SSI%29_Injection");
 /* Running all available modules for all available urls */
 urls.forEach(function(url, urlIndex, urls){
 	for(var moduleName in scanModules){
