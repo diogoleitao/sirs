d3ef3e975db5e786f89e953e5b2f9a3f62fd1d33
AnaCosteira <anacosteira.a21@gmail.com>
2014-12-05 17:25:00 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Run this script with the website to crawl as an argument and the Sirs.js will index and scan it
+# Make sure you have nodejs and npm installed
 echo Checking/Installing dependencies
 npm install
 mkdir results
 echo Indexing website
-node crawler/own.js http://testphp.vulnweb.com/ 50 > results/index
-# node crawler/own.js http://web.ist.utl.pt/ist173214/ 50 > results/index
+#node crawler/own.js http://testphp.vulnweb.com/ 50 > results/index
+node crawler/own.js http://web.ist.utl.pt/ist173214/ 50 > results/index
 echo Scanning website
 # We sort the output so the results are printed organized - NodeJS is an async utility
 node scanner/main.js < results/index | sort
