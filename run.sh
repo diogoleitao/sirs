5c85a9e5bc4152b07f1ffb05cf706249e401c35f
theburdencarrier <theburdencarrier@TheBurdenCarriers-MacBook-Pro.local>
2014-12-05 15:54:02 +0000

    SSI module working properly

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ npm install
 mkdir results
 echo Indexing website
 node crawler/own.js http://testphp.vulnweb.com/ 50 > results/index
-#node crawler/own.js http://web.ist.utl.pt/ist173214/ 50 > results/index
+# node crawler/own.js http://web.ist.utl.pt/ist173214/ 50 > results/index
 echo Scanning website
 # We sort the output so the results are printed organized - NodeJS is an async utility
 node scanner/main.js < results/index | sort
--- scanner/modules/server-side-includes-simple-ls.js
+++ scanner/modules/server-side-includes-simple-ls.js
@@ -1,123 +1,34 @@
 /*
 	Diogo Leitão
 	22:16 30/11/2014 
-	This modules simulates a SSI (server side includes) attack.
-	For simplicity purposes, this module only tests if a simple UNIX ls command
-	can be executed on a form
+	This modules simulates a SSI (server side includes) attack. It looks for the robots.txt file
 */
 var Url = require("url");
 var request = require('request');
-var cheerio = require("cheerio");
 module.exports = {
 	run: function (url) {
-		var injectableForms = [];
-
 		// Parsing the url and the queryString arguments
 		var urlObject = Url.parse(url, true);
-
-		// Get correspondent page		
-		request.get(urlObject.format(), function (err,httpResponse,body) {
-			var $;
-			if (httpResponse.statusCode == 200) {
-				// Get all forms in page via CSS selector powered by Cheerio
-				$ = cheerio.load(body);
-				var forms = $("form");
-
-				// Iterate through all forms and test them individually
-				forms.each(function (index, form) {
-					var action = $(this).attr("action");
-					var method = $(this).attr("method") || "GET";
-					var inputs = $(this).children(":input");
-					var actionUrlObject = Url.resolve(urlObject.format(), action);
-					// Iterate through all inputs and test them individually
-					var inputsArray = inputs.toArray();
-					inputsArray.forEach(function (index, testingInput) {
-						var testingInputName = $(inputsArray[testingInput]).attr("name");
-						var SSI = '<!--#exec cmd="ls" -->';
-						var form = {};
-						for(var input in inputsArray){
-							var inject = $(inputsArray[input]).attr("name") == testingInputName; 
-							form[$(inputsArray[input]).attr("name")] = (inject ? javaScript : ($(inputsArray[input]).val() == "" || $(inputsArray[input]).val() == undefined ? "default" : $(inputsArray[input]).val()));
+		Object.keys(urlObject.query).forEach(function (varName) {
+			var tries = 5;
+			// Making the request
+			var robots = "robots.txt";
+			for (var i = 0; i < tries; i++) {
+				// Appending robots.txt, default file for crawlers to ignore certain directories
+				urlObject.query[varName] = robots;
+				// Forcing update of final url
+				delete urlObject.search;
+				request({url: urlObject.format()}, function (err,httpResponse,body) {
+					if (httpResponse.statusCode == 200) {
+						if(body.toLowerCase().indexOf("example file".toLowerCase()) > -1){
+							console.log(url + " - SSI 'show robots.txt' Module: seems to be vulnerable to SSI attacks on variable " + varName);
 						}
-
-						if (method == "GET") {
-							var actionWithQuery = actionUrlObject + "?" + require('querystring').stringify(form);
-							request({
-								url: actionWithQuery
-							}, function (err, httpResponse, body) {
-								if (body.toLowerCase().indexOf(".php".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".py".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".pl".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".js".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".rb".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								}
-							});
-						} else {
-							request.post({
-								url: actionUrlObject,
-								form: form
-							}, function (err, httpResponse, body) {
-								if (body.toLowerCase().indexOf(".php".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".py".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".pl".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".js".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								} else if (body.toLowerCase().indexOf(".rb".toLowerCase()) > -1) {
-									if (!err) {
-										console.log(url + " - SSI Simple 'ls' command Module: Input with name " + testingInputName + " seems to be vulnerable to SSI");
-									} else {
-										console.log(url + " - SSI Simple 'ls' command Module: Testing input with name " + testingInputName + " caused error " + err + " on the server");
-									}
-								}
-							});
-						}
-					});
+					}
 				});
+				robots = '../' + robots;
 			}
+			// Cleaning up modifications to URL
+			urlObject = Url.parse(url, true);		
 		});
 	}
 }
