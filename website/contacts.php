5793d332b01e0d80da85a58fa1cef813d5be4a24
Ana Isabel Costeira Andrade Araujo <ist173214@linbase.rnl.tecnico.ulisboa.pt>
2014-12-05 15:50:01 +0000

    complete website

--- website/contacts.php
+++ website/contacts.php
@@ -24,21 +24,7 @@
 			<div>Contacts</div>
 		</a>
 	</nav>
-	<div class="feedback form">
-		<h3>Subscribe to our mailing list!</h3>
-		<form action="feedback.php" method="GET">
-			Name:<br><input type="text" id="name" name="name"><br>
-			Email:<br><input type="text" id="emai1" name="email"><br>
-			<input type="submit" value="Subscribe">
-		</form>
-	</div>
-	<div class = "login_form">
-		<form action="login.php" method="POST">
-			username:<br><input type="text" name="username"><br>
-			password:<br><input type="text" name="password"><br>
-			<input type="submit" value="Login">
-		</form>
-	</div>
+	
 	<div class = "WallTestimonial">
 		<form action="contacts.php" method="GET">
 			<p> Add your own testimonial!</p>
@@ -63,8 +49,11 @@
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	if ($username != NULL){
-		$sql = "INSERT INTO $tablename (name, test) VALUES ('$username','$test');";
-		$result = $db->query($sql);
+		$sql = $db->prepare("INSERT INTO $tablename (name, test) VALUES (:name, :test)");
+		$sql->bindParam('name', $username);
+		$sql->bindParam('test', $test);
+		
+		$sql->execute();
 	}
 	
 
